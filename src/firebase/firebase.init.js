8bd83ead21093f970b8688b9d2fd9a2f5bf1fd7c
grnisna <mdnisanahmed63@gmail.com>
2023-02-02 00:20:25 +0600

    set up project

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyClBS-7RvfNOYFE5kEqkBuStwRwAEGLo_c",
+  authDomain: "recap-ema-john-module-59.firebaseapp.com",
+  projectId: "recap-ema-john-module-59",
+  storageBucket: "recap-ema-john-module-59.appspot.com",
+  messagingSenderId: "589540147135",
+  appId: "1:589540147135:web:4142fb4341dd0a1d4d2b0a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
